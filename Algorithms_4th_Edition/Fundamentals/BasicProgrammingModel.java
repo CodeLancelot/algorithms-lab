e8ada46d86ae79a100626700058286a7d8b29ed0
DominicZoro <343151850@qq.com>
2019-06-23 19:12:35 +0800

    answer for exercise 1.1.13

--- Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
+++ Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
@@ -1,13 +1,31 @@
 package Fundamentals;
+
 import libraries.*;
+
 public class BasicProgrammingModel {
     public static void main(String[] args) {
-        double x = Double.parseDouble(args[0]), y = Double.parseDouble(args[1]);
-        if (x > 0 && x < 1 && y > 0 && y < 1) {
-            StdOut.println("true");
+        int m = 2, n = 3;
+        int[][] a = {{1,2,3},{1,2,3}}, b = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                b[j][i] = a[i][j];
+            }
         }
-        else {
-            StdOut.println("false");
+        printDArray(b);
+    }
+
+    public static void printDArray (int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        for (int i = 0; i < m; i++) {
+            StdOut.print('(');
+            for (int j = 0; j < n; j++) {
+                StdOut.print(arr[i][j]);
+                if (j < n-1) {
+                    StdOut.print(", ");
+                }
+            }
+            StdOut.println(')');
         }
     }
 }
