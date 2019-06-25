eeb4895d4234aab188e4726b80307844c26a8d00
Dominic <crazywizardhunter@gmail.com>
2019-06-25 14:26:50 +0800

    answer for exercise 1.1.18

--- Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
+++ Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
@@ -4,32 +4,23 @@ import libraries.*;
 
 public class BasicProgrammingModel {
     public static void main(String[] args) {
-        int m = 10;
-        int[] a = {1, 2, 2, 5, 1, 7, 5, 6, 1, 9};
-        Tools.printIntArray(histogram(a, m));
+        StdOut.println(multiply(2, 25));
+        StdOut.println(multiply(3, 11));
+        StdOut.println(multiply(19, 3));
+        StdOut.println(exponentCalculate(2, 5));
+        StdOut.println(exponentCalculate(3, 3));
+        StdOut.println(exponentCalculate(25, 2));
     }
 
-    public static int[] histogram(int[] arr, int m) {
-        int[] results = new int[m];
-        boolean isNeedExtraCheck = true;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= 0 && arr[i] < m) {
-                results[arr[i]]++;
-            } else {
-                isNeedExtraCheck = false;
-            }
-        }
-        if (isNeedExtraCheck) {
-            int sum = 0;
-            for (int i = 0; i < results.length; i++) {
-                sum += results[i];
-            }
-            if (sum == arr.length) {
-                StdOut.println("Fine");
-            } else {
-                StdOut.println("Illegal");
-            }
-        }
-        return results;
+    public static int multiply(int a, int b) {
+        if (b == 0) return 0;
+        if (b % 2 == 0) return multiply(a+a, b/2);
+        return multiply(a+a, b/2) + a;
+    }
+
+    public static int exponentCalculate(int a, int b) {
+        if (b == 0) return 1;
+        if (b % 2 == 0) return exponentCalculate(a*a, b/2);
+        return exponentCalculate(a*a, b/2) * a;
     }
 }
