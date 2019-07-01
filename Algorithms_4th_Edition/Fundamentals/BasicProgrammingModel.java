4d52dbf81913ef0b33f2e3692a279cda6e8b882d
DominicZoro <343151850@qq.com>
2019-07-01 22:58:30 +0800

    answer for exercise 1.1.31(Done)

--- Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
+++ Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
@@ -5,39 +5,49 @@ import libraries.*;
 
 public class BasicProgrammingModel {
     public static void main(String[] args) {
+        randomConnections(Integer.parseInt(args[0]), Double.parseDouble(args[1]));
+    }
+
+    public static void randomConnections(int n, double p) {
         //Use Polar Coordinate System
         //The coordinate of the polar point is (0, 0)
-        int n = 10, scale = 100;//Integer.parseInt(args[0])
+        int scale = 100;
         final int POLAR_RADIUS = 75;
         final double RAD = 2 * Math.PI;
-        double probability = 0.5;
-
         StdDraw.setXscale(-scale, scale);
         StdDraw.setYscale(-scale, scale);
         StdDraw.setPenRadius(.01);
 
-
         //The coordinate of the first point is (POLAR_RADIUS, 0)
-        //x is replaced with rcosθ，y is replaced with rsinθ, r^2=(x^2+y^2)
+        //x is replaced with rcos(theta)，y is replaced with rsin(theta), r^2=(x^2+y^2)
         double polarAngle = 0;
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
             polarAngle = (double) i / n * RAD;
             double x = POLAR_RADIUS * Math.cos(polarAngle);
             double y = POLAR_RADIUS * Math.sin(polarAngle);
+            StdOut.printf("(%.2f, %.2f)\n", x, y);
             points[i] = new Point();
             points[i].setLocation(x, y);
             StdDraw.point(x, y);
         }
 
-        for (int i = 1; i < n; i++) {
-            StdOut.printf("(%.2f, %.2f)\n", points[i].getX(), points[i].getY());
-            StdDraw.line(points[0].getX(), points[0].getY() , points[i].getX(), points[i].getY());
-        }
-
+        drawLinesAmongPoints(points, p);
     }
 
-    public static void randomConnections() {} {
+    public static void drawLinesAmongPoints(Point[] points, double probability) {
+        if (points.length  <= 1) {
+            return;
+        }
 
+        int len = points.length;
+        Point[] untreatedPoints = new Point[len-1];
+        for (int i = 1; i < len; i++) {
+            if (StdRandom.random() <= probability) {
+                StdDraw.line(points[0].getX(), points[0].getY() , points[i].getX(), points[i].getY());
+            }
+            untreatedPoints[i-1] = points[i];
+        }
+        drawLinesAmongPoints(untreatedPoints, probability);
     }
 }
