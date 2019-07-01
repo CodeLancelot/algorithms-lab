8fed74f13301a78e9672508850c3363a7abca26c
Dominic <crazywizardhunter@gmail.com>
2019-07-01 17:59:53 +0800

    answer for exercise 1.1.31(Not done yet)

--- Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
+++ Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
@@ -1,27 +1,43 @@
 package Fundamentals;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Point;
 import libraries.*;
 
 public class BasicProgrammingModel {
     public static void main(String[] args) {
-        List<String> tableData = new ArrayList<String>();
-        while (StdIn.hasNextLine()) {
-            tableData.add(StdIn.readLine());
+        //Use Polar Coordinate System
+        //The coordinate of the polar point is (0, 0)
+        int n = 10, scale = 100;//Integer.parseInt(args[0])
+        final int POLAR_RADIUS = 75;
+        final double RAD = 2 * Math.PI;
+        double probability = 0.5;
+
+        StdDraw.setXscale(-scale, scale);
+        StdDraw.setYscale(-scale, scale);
+        StdDraw.setPenRadius(.01);
+
+
+        //The coordinate of the first point is (POLAR_RADIUS, 0)
+        //x is replaced with rcosθ，y is replaced with rsinθ, r^2=(x^2+y^2)
+        double polarAngle = 0;
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            polarAngle = (double) i / n * RAD;
+            double x = POLAR_RADIUS * Math.cos(polarAngle);
+            double y = POLAR_RADIUS * Math.sin(polarAngle);
+            points[i] = new Point();
+            points[i].setLocation(x, y);
+            StdDraw.point(x, y);
         }
-        printTable(tableData);
+
+        for (int i = 1; i < n; i++) {
+            StdOut.printf("(%.2f, %.2f)\n", points[i].getX(), points[i].getY());
+            StdDraw.line(points[0].getX(), points[0].getY() , points[i].getX(), points[i].getY());
+        }
+
     }
 
-    public static void printTable(List<String> tableData) {
-        StdOut.println("[Player Name] [Total Points] [Count] [Average Score]");
-        tableData.forEach(rowStr -> {
-            String[] row = rowStr.split(" ");
-            String name = row[0];
-            int points = Integer.parseInt(row[1]);
-            int count = Integer.parseInt(row[2]);
-            double score = (double)points/(double)count;
-            StdOut.printf("[%-11s] [%12d] [%5d] [%13.3f]\n", name, points, count, score);
-        });
+    public static void randomConnections() {} {
+
     }
 }
