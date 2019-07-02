65abd359cfff9e9e71d52f188e3b981076d16918
Dominic <crazywizardhunter@gmail.com>
2019-07-02 16:40:07 +0800

    experiment 1.1.35

--- Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
+++ Algorithms_4th_Edition/Fundamentals/BasicProgrammingModel.java
@@ -1,53 +1,35 @@
 package Fundamentals;
 
 import java.awt.Point;
+
 import libraries.*;
 
 public class BasicProgrammingModel {
     public static void main(String[] args) {
-        randomConnections(Integer.parseInt(args[0]), Double.parseDouble(args[1]));
+        diceSimulation(Integer.parseInt(args[0]));
     }
 
-    public static void randomConnections(int n, double p) {
-        //Use Polar Coordinate System
-        //The coordinate of the polar point is (0, 0)
-        int scale = 100;
-        final int POLAR_RADIUS = 75;
-        final double RAD = 2 * Math.PI;
-        StdDraw.setXscale(-scale, scale);
-        StdDraw.setYscale(-scale, scale);
-        StdDraw.setPenRadius(.01);
-
-        //The coordinate of the first point is (POLAR_RADIUS, 0)
-        //x is replaced with rcos(theta)，y is replaced with rsin(theta), r^2=(x^2+y^2)
-        double polarAngle = 0;
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            polarAngle = (double) i / n * RAD;
-            double x = POLAR_RADIUS * Math.cos(polarAngle);
-            double y = POLAR_RADIUS * Math.sin(polarAngle);
-            StdOut.printf("(%.2f, %.2f)\n", x, y);
-            points[i] = new Point();
-            points[i].setLocation(x, y);
-            StdDraw.point(x, y);
+    public static void diceSimulation (int N) {
+        //N is at least at ten million level, I met the request when first trying 60000000
+        int SIDES = 6;
+        double[] dist = new double[2 * SIDES + 1];
+        for (int i = 1; i <= SIDES; i++) {
+            for (int j = 1; j <= SIDES; j++)
+                dist[i + j]++;
         }
+        for (int k = 2; k <= 2 * SIDES; k++)
+            dist[k] /= 36.0;
+        Tools.printArray(dist, 3);
 
-        drawLinesAmongPoints(points, p);
-    }
 
-    public static void drawLinesAmongPoints(Point[] points, double probability) {
-        if (points.length  <= 1) {
-            return;
+        double[] test = new double[2 * SIDES + 1];
+        for (int i = 0; i < N; i++) {
+            int dice1 = StdRandom.uniform(SIDES) + 1;
+            int dice2 = StdRandom.uniform(SIDES) + 1;
+            test[dice1 + dice2]++;
         }
-
-        int len = points.length;
-        Point[] untreatedPoints = new Point[len-1];
-        for (int i = 1; i < len; i++) {
-            if (StdRandom.random() <= probability) {
-                StdDraw.line(points[0].getX(), points[0].getY() , points[i].getX(), points[i].getY());
-            }
-            untreatedPoints[i-1] = points[i];
-        }
-        drawLinesAmongPoints(untreatedPoints, probability);
+        for (int k = 2; k <= 2 * SIDES; k++)
+            test[k] /= N;
+        Tools.printArray(test, 3);
     }
 }
--- Algorithms_4th_Edition/Fundamentals/Tools.java
+++ Algorithms_4th_Edition/Fundamentals/Tools.java
@@ -11,18 +11,19 @@ public class Tools {
                 StdOut.print(' ');
             }
         }
-        StdOut.print(']');
+        StdOut.println(']');
     }
 
-    public static void printArray(double[] arr) {
+    public static void printArray(double[] arr, int precision) {
         StdOut.print('[');
         for (int i = 0; i < arr.length; i++) {
-            StdOut.print(arr[i]);
+            String formatStr = "%." + precision + 'f';
+            StdOut.printf(formatStr, arr[i]);
             if (i != arr.length - 1) {
                 StdOut.print(' ');
             }
         }
-        StdOut.print(']');
+        StdOut.println(']');
     }
 
     public static void printArray(String[] arr) {
@@ -33,7 +34,7 @@ public class Tools {
                 StdOut.print(' ');
             }
         }
-        StdOut.print(']');
+        StdOut.println(']');
     }
 
     public static void printTwoDimensionalArray(int[][] arr) {
@@ -51,13 +52,14 @@ public class Tools {
         }
     }
 
-    public static void printTwoDimensionalArray(double[][] arr) {
+    public static void printTwoDimensionalArray(double[][] arr, int precision) {
         int m = arr.length;
         int n = arr[0].length;
         for (int i = 0; i < m; i++) {
             StdOut.print('(');
             for (int j = 0; j < n; j++) {
-                StdOut.print(arr[i][j]);
+                String formatStr = "%." + precision + 'f';
+                StdOut.printf(formatStr, arr[i][j]);
                 if (j < n - 1) {
                     StdOut.print(", ");
                 }
