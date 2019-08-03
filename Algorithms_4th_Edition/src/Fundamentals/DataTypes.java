7d2f38a2ee2ff2eb2440d8fc6d68c9b5836b9d77
DominicZoro <343151850@qq.com>
2019-08-03 12:47:47 +0800

    answer for exercise 1.3.10

--- Algorithms_4th_Edition/src/Fundamentals/DataTypes.java
+++ Algorithms_4th_Edition/src/Fundamentals/DataTypes.java
@@ -6,20 +6,56 @@ public class DataTypes {
     //the bag, the queue, and the stack
 
     public static void main(String[] args) {
-        Stack<String> vStack = new Stack<>();
+        StdOut.println(convertInfixToPostfix());
+    }
+
+    static String convertInfixToPostfix() {
+        String postfix = "";
         Stack<String> oStack = new Stack<>();
         while (!StdIn.isEmpty()) {
             String str = StdIn.readString();
-            if (str.equals(")")) {
-                String value1 = vStack.pop(), value2 = vStack.pop(), operator = oStack.pop();
-                String value = "(" + value2 + " " + operator + " " + value1 + ")";
-                vStack.push(value);
-            } else if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
+            if (str.equals("(")) {
+                oStack.push("(");
+            } else if (str.equals(")")) {
+                String op = oStack.pop();
+                while (!op.equals("(")) {
+                    postfix += op;
+                    op = oStack.pop();
+                }
+            } else if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/") || str.equals("^")) {
+                while (oStack.size() != 0) {
+                    if (hasHigherPrecedence(str, oStack)) {
+                        break;
+                    } else {
+                        postfix += oStack.pop();
+                    }
+                }
                 oStack.push(str);
             } else {
-                vStack.push(str);
+                postfix += str;
             }
         }
-        StdOut.println(vStack.pop());
+
+        while (oStack.size() != 0) {
+            postfix += oStack.pop();
+        }
+        return postfix;
+    }
+
+    private static boolean hasHigherPrecedence(String str, Stack<String> oStack) {
+        int p1 = precedence(str), p2 = precedence(oStack.peek());
+        return p1 > p2;
+    }
+
+    private static int precedence(String operator) {
+        int precedence = 0;
+        if (operator.equals("^")) {
+            precedence = 5;
+        } else if (operator.equals("*") || operator.equals("/")) {
+            precedence = 3;
+        } else if (operator.equals("+") || operator.equals("-")) {
+            precedence = 2;
+        }
+        return precedence;
     }
 }
