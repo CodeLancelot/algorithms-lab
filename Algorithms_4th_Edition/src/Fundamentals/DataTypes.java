075d409326ce696f49f41407134f31ca0092b16c
DominicZoro <343151850@qq.com>
2019-08-10 20:51:38 +0800

    answer for exercise 1.3.43

--- Algorithms_4th_Edition/src/Fundamentals/DataTypes.java
+++ Algorithms_4th_Edition/src/Fundamentals/DataTypes.java
@@ -2,40 +2,31 @@ package Fundamentals;
 
 import libraries.*;
 
+import java.io.File;
+
 public class DataTypes {
     //the bag, the queue, and the stack
 
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        int M = Integer.parseInt(args[1]);
+        printFileList(args[0], 0);
+    }
 
-        CircularLinkedList<Integer> list = new CircularLinkedList<>();
-        list.print();
-
-        for (int i = 0; i < N; i++) {
-            list.insertRear(i);
-        }
-        list.print();
-
-        int nodeIndex = 0, numberOff = 0;
-        while (!list.isEmpty()) {
-            if (numberOff < M - 1) {
-                numberOff++;
-                if (!list.isRear(nodeIndex)) {
-                    nodeIndex++;
-                } else {
-                    nodeIndex = 0;
+    static void printFileList(String filePath, int indent) {
+        File folder = new File(filePath);
+        if (folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            for (File file : files) {
+                for (int i = indent; i > 0; i--) {
+                    StdOut.print("  ");
                 }
-            } else {
-                Node<Integer> node = list.getNode(nodeIndex);
-                if (list.isRear(node)) {
-                    nodeIndex = 0;
+                if (file.isFile()) {
+                    StdOut.println(file.getName());
+                } else if (file.isDirectory()) {
+                    StdOut.println(file.getName());
+                    printFileList(file.getPath(), indent + 1);
                 }
-                StdOut.print(node.item + " ");
-                list.removeNode(node);
-                numberOff = 0;
             }
         }
-        StdOut.println();
     }
+
 }
