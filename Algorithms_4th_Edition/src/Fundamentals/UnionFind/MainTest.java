a3e9605f84b6a4c26ffa1d62d0e3f5d0cefa24ef
Dominic <crazywizardhunter@gmail.com>
2019-09-19 16:55:35 +0800

    Union-Find weighted-quick-union

--- Algorithms_4th_Edition/src/Fundamentals/UnionFind/MainTest.java
+++ Algorithms_4th_Edition/src/Fundamentals/UnionFind/MainTest.java
@@ -6,14 +6,17 @@ public class MainTest {
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]); // N sites.
         QuickUnion quickUnion = new QuickUnion(N);
+        WeightedQuickUnion weightedQuickUnion = new WeightedQuickUnion(N);
         while (!StdIn.isEmpty()) {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
             if (quickUnion.connected(p, q)) continue; // Ignore if connected.
             quickUnion.union(p, q); // Combine components
+            weightedQuickUnion.union(p, q);
             StdOut.println("A new connection created: " + p + " " + q);
         }
         StdOut.println(quickUnion.count() + " components");
         quickUnion.printAllComponents();
+        weightedQuickUnion.printAllSitesPath();
     }
 }
---
+++ Algorithms_4th_Edition/src/Fundamentals/UnionFind/WeightedQuickUnion.java
@@ -0,0 +1,61 @@
+package Fundamentals.UnionFind;
+
+import libraries.StdOut;
+
+public class WeightedQuickUnion implements UnionFind {
+    private int[] links;
+    private int[] sizes;
+    private int count;
+
+    WeightedQuickUnion(int N) {
+        count = N;
+        links = new int[N];
+        sizes = new int[N];
+        for (int i = 0; i < N; i++) {
+            links[i] = i;
+            sizes[i] = 1;
+        }
+    }
+
+    public int find(int p) {
+        while (p != links[p]) {
+            p = links[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) return;
+        if (sizes[pRoot] < sizes[qRoot]) {
+            links[pRoot] = qRoot;
+            sizes[qRoot] += sizes[pRoot];
+        } else {
+            links[qRoot] = pRoot;
+            sizes[pRoot] += sizes[qRoot];
+        }
+        count--;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int count() {
+        return count;
+    }
+
+    void printAllSitesPath() {
+        for (int i = 0; i < links.length; i++) {
+            int j = i;
+            while (j != links[j]) {
+                StdOut.print(j + " ");
+                j = links[j];
+            }
+            StdOut.print(j + " ");
+            StdOut.print("( Component " + j + " )");
+            StdOut.println();
+        }
+    }
+}
