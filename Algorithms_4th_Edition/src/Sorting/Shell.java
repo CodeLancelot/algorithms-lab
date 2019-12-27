a0b4d18ae2dd676d7e3ab18e0a069cfaf9a164cd
DominicZoro <343151850@qq.com>
2019-12-27 22:32:15 +0800

    Shell sort

---
+++ Algorithms_4th_Edition/src/Sorting/Shell.java
@@ -0,0 +1,16 @@
+package Sorting;
+
+public class Shell extends Sort {
+    public static void sort(Comparable[] arr) {
+        int N = arr.length;
+        int h = 1;
+        while (h < N / 3) h = h * 3 + 1;
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(arr[j], arr[j - h]); j -= h)
+                    exch(arr, j, j - h);
+            }
+            h /= 3;
+        }
+    }
+}
