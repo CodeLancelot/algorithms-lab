eece993b4a5a3b9e15dd3cfffaac754cd4e325d3
DominicZoro <343151850@qq.com>
2020-02-09 15:01:34 +0800

    Quick sort with 3-way partitioning

--- Algorithms_4th_Edition/src/Sorting/Quick.java
+++ Algorithms_4th_Edition/src/Sorting/Quick.java
@@ -10,11 +10,6 @@ public class Quick {
     private Quick() {
     }
 
-    public static void sort(Comparable[] a) {
-        StdRandom.shuffle(a);
-        sort(a, 0, a.length - 1);
-    }
-
     private static int partition(Comparable[] a, int lo, int hi) {
         int i = lo, j = hi + 1;
         int m = medianIndex(a, lo, (lo + hi) / 2, hi);
@@ -48,13 +43,37 @@ public class Quick {
         sort(a, p + 1, hi);
     }
 
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sortDuplicateKeys(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int lt = lo, i = lo + 1, gt = hi;
+        Comparable v = a[lo];
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) Sort.exch(a, lt++, i++);
+            else if (cmp > 0) Sort.exch(a, i, gt--);
+            else i++;
+        }
+        sortDuplicateKeys(a, lo, lt - 1);
+        sortDuplicateKeys(a, gt + 1, hi);
+    }
+
+    public static void sortDuplicateKeys(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sortDuplicateKeys(a, 0, a.length - 1);
+    }
+
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
         Integer[] a = new Integer[N];
         for (int i = 0; i < N; i++)
-            a[i] = StdRandom.uniform(N);
+            a[i] = StdRandom.uniform(3);
 
-        sort(a);
+        sortDuplicateKeys(a);
         assert Sort.isSorted(a);
         Tools.printArray(a);
     }
